dc5c868d4f5db6a738582c3b63d0144fbf68cbad
yahupc <yahu39pc@gmail.com>
2023-12-01 01:39:14 -0500

    Creacion Repository

--- SpringBootRest/src/main/java/com/application/rest/repository/MakerRepository.java
+++ SpringBootRest/src/main/java/com/application/rest/repository/MakerRepository.java
@@ -1,2 +1,14 @@
+<<<<<<< HEAD
 package com.application.rest.repository;public interface MakerRepository {
 }
+=======
+package com.application.rest.repository;
+
+import com.application.rest.entities.Maker;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MakerRepository extends CrudRepository<Maker, Long> {
+}
+>>>>>>> 8f7493f (Creacion Repository)
